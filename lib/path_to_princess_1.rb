c089408b6601cffa28a1399d158174a0697305de
Tylor Schafer <tylorschafer@gmail.com>
2019-12-10 22:29:16 -0700

    Add working find_character_location method

--- lib/path_to_princess_1.rb
+++ lib/path_to_princess_1.rb
@@ -1,6 +1,8 @@
 class PathToPrincess1
 
   def find_character_location(grid, character)
-    
+    vertical_index = grid.find_index { |row| row.include?(character) }
+    horizontal_index = grid[vertical_index].index(character)
+    [vertical_index, horizontal_index]
   end
 end
