bc6ebf4da3a0d4fd95cf5a15d95e1f32617e8cf0
Tylor Schafer <tylorschafer@gmail.com>
2019-12-10 22:25:29 -0700

    Change find_character to find_character_location

--- spec/path_to_princess_1_spec.rb
+++ spec/path_to_princess_1_spec.rb
@@ -1,20 +1,21 @@
 require 'spec_helper'
 
-describe 'PathToPrincess1' do
+RSpec.describe 'PathToPrincess1' do
   before :each do
     @game = PathToPrincess1.new
   end
+
   describe '#find_character_location' do
     it 'finds the index of both array dimensions' do
       grid = [['p','-','-'], ['-','m','-'], ['-','-','-']]
 
-      expect(@game.find_character(grid, 'm')).to eq([1,1])
-      expect(@game.find_character(grid, 'p')).to eq([0,0])
+      expect(@game.find_character_location(grid, 'm')).to eq([1,1])
+      expect(@game.find_character_location(grid, 'p')).to eq([0,0])
 
       grid = [['-','-','-'], ['m','-','-'], ['-','-','p']]
 
-      expect(@game.find_character(grid, 'm')).to eq([1,0])
-      expect(@game.find_character(grid, 'p')).to eq([2,2])
+      expect(@game.find_character_location(grid, 'm')).to eq([1,0])
+      expect(@game.find_character_location(grid, 'p')).to eq([2,2])
     end
   end
 end
