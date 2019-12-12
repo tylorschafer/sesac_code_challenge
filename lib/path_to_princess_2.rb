98f7553b8cea721bb2a9df93ab9965bcb2b47714
Tylor Schafer <tylorschafer@gmail.com>
2019-12-12 11:44:24 -0700

    Add nextMove method, all tests passing

--- lib/path_to_princess_2.rb
+++ lib/path_to_princess_2.rb
@@ -3,4 +3,11 @@ require './modules/step_finder'
 class PathToPrincess2
   include StepFinder
 
+  def nextMove(n,r,c,grid)
+    mario = { vertical: r, horizontal: c }
+    peach = findCharacterLocation(grid, 'p')
+    steps = findSteps(mario, peach)
+    print directions(steps).first
+    return directions(steps).first
+  end
 end
