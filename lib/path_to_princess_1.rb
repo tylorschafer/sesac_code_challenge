1c855d5a9745e94f3b4bbebb90e567503dd9453b
Tylor Schafer <tylorschafer@gmail.com>
2019-12-12 10:01:43 -0700

    Add findCharacterLocation and findSteps methods to StepFinder Module

--- lib/path_to_princess_1.rb
+++ lib/path_to_princess_1.rb
@@ -1,16 +1,7 @@
+require './modules/step_finder'
+
 class PathToPrincess1
-
-  def findCharacterLocation(grid, character)
-    vertical_index = grid.find_index { |row| row.include?(character) }
-    horizontal_index = grid[vertical_index].index(character)
-    { vertical: vertical_index, horizontal: horizontal_index }
-  end
-
-  def findSteps(hero, damsel)
-    vertical = hero[:vertical] - damsel[:vertical]
-    horizontal = hero[:horizontal] - damsel[:horizontal]
-    { vertical: vertical, horizontal: horizontal }
-  end
+  include StepFinder
 
   def takeSteps(steps)
     output = ''
